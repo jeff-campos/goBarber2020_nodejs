91a9b0c5cf67b47377692372da771ff47a52d2ef
Admin <admin@jeff-campos.local>
2020-11-19 12:30:20 -0300

    initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '664f6efc531849fdcca6844d6a403163a',
+    expiresIn: '1d',
+  },
+};
---
+++ src/repositories/AppointmentRepository.ts
@@ -0,0 +1,14 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Appointment from '../models/Appointment';
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppontment = await this.findOne({
+      where: { date },
+    });
+    return findAppontment || null;
+  }
+}
+
+export default AppointmentsRepository;
