670401680941e25553dd642a5ad890ea98831a41
Admin <admin@jeff-campos.local>
2021-01-23 11:23:18 -0300

    organization of the code to use DDD

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import CreateAppointmentService from '../service/CreateAppointmentService';
 
-import AppointmentsRepository from '../repositories/AppointmentRepository';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
+import AppointmentsRepository from '@modules/appointments/repositories/AppointmentRepository';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
 
